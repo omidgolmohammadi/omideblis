b37f2d1d4410cc84fb448035f0d2ec5258a2822a
omidgolmohammadi <omidgolmohammadi@users.noreply.github.com>
2016-04-24 19:38:37 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,20 +1,15 @@
 --------------------------------------------------
---      ____  ____ _____                        --
---     |    \|  _ )_   _|___ ____   __  __      --
---     | |_  )  _ \ | |/ ·__|  _ \_|  \/  |     --
---     |____/|____/ |_|\____/\_____|_/\/\_|     --
 --                                              --
---------------------------------------------------
---                                              --
---       Developers: @Josepdal & @MaSkAoS       --
---     Support: @Skneos,  @iicc1 & @serx666     --
+--       Developers: @heset_ni_zendgi @Xx_admin_eblis_xX      --
+--     our chanal:  @eblis_unity   --
 --                                              --
 --------------------------------------------------
 
 do
 
 function run(msg, matches)
-  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot\n\nDevelopers: @MaSkAoS @Josepdal\nSupport: @skneos @iicc1 @Serx666\nChannels: @DBTeamEN @DBTeamES\n\nCheckout: https://github.com/Josepdal/DBTeam\nGNU GPL v2 license.'
+  return '❄️Freeze robot V1❄️\n                                                                        💥created by Eblis team💥\n\n 🏅 sudo users 🏅                                                                                                                                                         @heset_ni_zendgi                                               @Xx_admin_eblis_xX                                                                             ️\n ➡️ our chanal: @eblis_unity⬅                                                       ️〰〰〰〰〰〰〰〰〰〰〰'
+  
 end
 
 return {
