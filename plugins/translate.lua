3cdebe70bb1210d851f525ab27b11a8b0c983639
omidgolmohammadi <omidgolmohammadi@users.noreply.github.com>
2016-05-12 14:50:45 +0430

    Update translate.lua

--- plugins/translate.lua
+++ plugins/translate.lua
@@ -53,14 +53,14 @@ end
 return {
   description = "Translate some text", 
   usage = {
-    "[/!]tr text. Translate the text to English.",
-    "[/!]tra target_lang text.",
-    "[/!]tr source.target text",
+    "[/!#]tr text. Translate the text to English.",
+    "[/!#]tra target_lang text.",
+    "[/!#]tr source.target text",
   },
   patterns = {
-    "^[/!]tr ([%w]+).([%a]+) (.+)",
-    "^[/!]tr ([%w]+) (.+)",
-    "^[/!]tr (.+)",
+    "^[/!#]tr ([%w]+).([%a]+) (.+)",
+    "^[/!#]tr ([%w]+) (.+)",
+    "^[/!#]tr (.+)",
   }, 
   run = run 
 }
