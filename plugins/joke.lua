72bcd14dd96ab6099a35347264388532e241d1be
omidgolmohammadi <omidgolmohammadi@users.noreply.github.com>
2016-05-12 15:53:28 +0430

    Update joke.lua

--- plugins/joke.lua
+++ plugins/joke.lua
@@ -1,7 +1,7 @@
 local database = 'http://alirezabot.uk.to/'
 local function run(msg)
 local res = http.request(database.."joke.db")
-local joke = res:split(",")
+local joke = 'res':split(",")
 return joke[math.random(#joke)]
 end
 
