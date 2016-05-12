ab39dc3310a410fc87f86e0e312d19b82ca439e2
omidgolmohammadi <omidgolmohammadi@users.noreply.github.com>
2016-05-12 14:57:37 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -8,7 +8,7 @@
 do
 
 function run(msg, matches)
-  return '❄️Freeze robot V1❄️\n                                                                        💥created by Eblis team💥\n\n 🏅 sudo users 🏅                                                                                                                                                         @heset_ni_zendgi                                               @Xx_admin_eblis_xX                                                                             ️\n ➡️ our chanal: @eblis_unity⬅                                                       ️〰〰〰〰〰〰〰〰〰〰〰'
+  return 'anti spam robott V1\n                                                                        💥created by omid programer and khan💥\n\n 🏅 sudo users 🏅                                                                                                                                                       @omid_programer                                           @HBKHAN                                                                           ️\n ➡️ our chanal: @H_Ghost_Life⬅                                                       ️〰〰〰〰〰〰〰〰〰〰〰'
   
 end
 
